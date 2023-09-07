798d99e976812625bba579487639015f9b8d2a69
Sasha Czerniawski <sashacandoit@gmail.com>
2023-09-07 14:53:43 -0400

    changing boilerplate functions in DocEditor

--- src/DocEditor.js
+++ src/DocEditor.js
@@ -3,23 +3,23 @@ import DocEditorForm from "./DocEditorForm";
 import EmploymentContract from "./EmploymentContract";
 import "./DocEditor.css"
 
-const MadLib = () => {
-    /** initialize state for madlib words */
+const DocEditor = () => {
+    /** initialize state for doc words */
     const [words, setWords] = useState([]);
 
     /** takes form data and adds new words to state */
-    const getSongWords = (formWords) => {
+    const getDocWords = (formWords) => {
         setWords(...words, {...formWords})
     }
     return (
         <div className="MadLib">
             <h1 className="Doc-title">Employment Contract</h1>
             <h4 className="Doc-subtitle">Fill in the blanks below to add to the Employment Contract</h4>
-            <DocEditorForm getSongWords={getSongWords} />
+            <DocEditorForm getSongWords={getDocWords} />
             <EmploymentContract storyWords={words} />
         </div>
     )
 }
 
 
-export default MadLib;
+export default DocEditor;
