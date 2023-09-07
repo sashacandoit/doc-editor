2530c391eb36afab9f459a30bf531dc73fb4b180
Sasha Czerniawski <sashacandoit@gmail.com>
2023-09-07 14:59:04 -0400

    change boilerplate items in form fields

--- src/EmploymentContractFields.js
+++ src/EmploymentContractFields.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid';
 
 const employmentContractFields = {
-    title: "Rudolph the MadLibbed Reindeer",
+    title: "Employment Contract",
     words: [
         { word: 'name1', label: 'Name:', value: '', placeholder: 'Prancer, Tom, etc.', id: uuid() },
         { word: 'name2', label: 'Name:', value: '', placeholder: 'Comet, Penelope, etc.', id: uuid() },
@@ -19,29 +19,8 @@ const employmentContractFields = {
         { word: 'verb4', label: 'Verb:', value: '', placeholder: 'Past Tense - loved, tickled, etc', id: uuid() },
         { word: 'verb5', label: 'Verb:', value: '', placeholder: 'Past Tense - shouted, slept, etc', id: uuid() },
         { word: 'color', label: 'Color:', value: '', placeholder: 'same color as before!', id: uuid() }
-    ],
-    // song: [
-    //     `You know Dasher and Dancer and `, `___`, ' and Vixen,',
-    //     `___`, ' and ', `___`, ' and ', `___`, ' and ', 'Blitzen',
-    //     'But do you recall,',
-    //     'The most ', `___`, ' reindeer of all?',
-    //     'Rudolph the', `___`, '-nosed reindeer',
-    //     'Had a very ', `___`, 'nose,',
-    //     'And if you ever ', `___`, 'it,',
-    //     'You would even say it ', `___`, '.',
-    //     'All of the other reindeer',
-    //     'used to ', `___`, ' and call him names',
-    //     'They never let poor Rudolph',
-    //     `___`, ' in any reindeer games.',
-    //     'Then one ', `___`, ' ', `___`,
-    //     'Santa came to say,',
-    //     `"Rudolph with your nose so `, `___`, ',',
-    //     `Won't you `, '___', ' my ', `___` `tonight?"`,
-    //     'then how the reindeer ', `___`, 'him,',
-    //     'As they ', `___`, ' out with glee,',
-    //     `"Rudolph the `, `___`, '-nosed reindeer,',
-    //     `You'll go down in history!"`
-    // ]
+    ]
+    
 }
 
 export { employmentContractFields };
